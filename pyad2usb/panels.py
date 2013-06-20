5fa88fa503bb7e383fb0918beeef49c7802a8b43
Scott Petersen <scott@nutech.com>
2013-06-20 11:59:14 -0700

    Why wasnt this already commited? heh

---
+++ pyad2usb/panels.py
@@ -0,0 +1,13 @@
+"""
+Representations of Panels and their templates.
+"""
+
+VISTA20 = 0
+
+TEMPLATES = {
+    VISTA20: {
+        'name': 'Vista 20',
+        # number of expanders, starting_address, number of channels
+        'expanders': (5, 7, 7)
+    }
+}
