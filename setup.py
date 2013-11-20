5942b087dec3ba96adbfbea1082f152de40b05e3
Scott Petersen <scott@nutech.com>
2013-11-20 15:05:55 -0800

    Release prep.

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='pyad2',
+    version='0.5',
+    description='Interface to the AD2 family of alarm devices.',
+    long_description=readme(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Security Alarms',
+    ],
+    keywords='ad2usb ad2serial ad2pi alarm security ademco',
+    url='http://github.com/nutechsoftware/pyad2',
+    author='Nu Tech Software Solutions, Inc.',
+    author_email='general@support.nutech.com',
+    license='',
+    packages=['pyad2'],
+    install_requires=[
+        'OpenSSL',
+        'pyftdi'
+    ],
+    include_package_data=True,
+    zip_safe=False)
