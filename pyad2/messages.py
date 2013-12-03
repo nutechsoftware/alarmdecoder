0d76104f2cb52ef34bfbe81837554af785d4194b
Scott Petersen <scott@nutech.com>
2013-12-03 10:51:09 -0800

    Moved instance variables into the classes for ease of documentation.

--- pyad2/messages.py
+++ pyad2/messages.py
@@ -12,11 +12,15 @@ class BaseMessage(object):
     """
     Base class for messages.
     """
+
+    raw = None
+    """The raw message text"""
+
     def __init__(self):
         """
         Constructor
         """
-        self.raw = None
+        pass
 
     def __str__(self):
         """
@@ -29,6 +33,51 @@ class Message(BaseMessage):
     Represents a message from the alarm panel.
     """
 
+    ready = False
+    """Indicates whether or not the panel is in a ready state"""
+    armed_away = False
+    """Indicates whether or not the panel is armed away"""
+    armed_home = False
+    """Indicates whether or not the panel is armed home"""
+    backlight_on = False
+    """Indicates whether or not the keypad backlight is on"""
+    programming_mode = False
+    """Indicates whether or not we're in programming mode"""
+    beeps = -1
+    """Number of beeps associated with a message"""
+    zone_bypassed = False
+    """Indicates whether or not a zone is bypassed"""
+    ac_power = False
+    """Indicates whether or not the panel is on AC power"""
+    chime_on = False
+    """Indicates whether or not the chime is enabled"""
+    alarm_event_occurred = False
+    """Indicates whether or not an alarm event has occurred"""
+    alarm_sounding = False
+    """Indicates whether or not an alarm is sounding"""
+    battery_low = False
+    """Indicates whether or not there is a low battery"""
+    entry_delay_off = False
+    """Indicates whether or not the entry delay is enabled"""
+    fire_alarm = False
+    """Indicates whether or not a fire alarm is sounding"""
+    check_zone = False
+    """Indicates whether or not there are zones that require attention."""
+    perimeter_only = False
+    """Indicates whether or not the perimeter is armed"""
+    numeric_code = None
+    """The numeric code associated with the message"""
+    text = None
+    """The human-readable text to be displayed on the panel LCD"""
+    cursor_location = -1
+    """Current cursor location on the keypad"""
+    mask = None
+    """Address mask this message is intended for"""
+    bitfield = None
+    """The bitfield associated with this message"""
+    panel_data = None
+    """The panel data field associated with this message"""
+
     def __init__(self, data=None):
         """
         Constructor
@@ -36,34 +85,18 @@ class Message(BaseMessage):
         :param data: Message data to parse.
         :type data: str
         """
-        self.ready = False
-        self.armed_away = False
-        self.armed_home = False
-        self.backlight_on = False
-        self.programming_mode = False
-        self.beeps = -1
-        self.zone_bypassed = False
-        self.ac_power = False
-        self.chime_on = False
-        self.alarm_event_occurred = False
-        self.alarm_sounding = False
-        self.battery_low = False
-        self.entry_delay_off = False
-        self.fire_alarm = False
-        self.check_zone = False
-        self.perimeter_only = False
-        self.numeric_code = ""
-        self.text = ""
-        self.cursor_location = -1
-        self.data = ""
-        self.mask = ""
-        self.bitfield = ""
-        self.panel_data = ""
+        BaseMessage.__init__(self)
 
         self._regex = re.compile('("(?:[^"]|"")*"|[^,]*),("(?:[^"]|"")*"|[^,]*),("(?:[^"]|"")*"|[^,]*),("(?:[^"]|"")*"|[^,]*)')
 
         if data is not None:
             self._parse_message(data)
+
+    def __str__(self):
+        """
+        String conversion operator.
+        """
+        return self.raw
 
     def _parse_message(self, data):
         """
@@ -107,12 +140,6 @@ class Message(BaseMessage):
         if int(self.panel_data[19:21], 16) & 0x01 > 0:
             self.cursor_location = int(self.bitfield[21:23], 16)    # Alpha character index that the cursor is on.
 
-    def __str__(self):
-        """
-        String conversion operator.
-        """
-        return self.raw
-
 class ExpanderMessage(BaseMessage):
     """
     Represents a message from a zone or relay expansion module.
@@ -123,6 +150,16 @@ class ExpanderMessage(BaseMessage):
     RELAY = 1
     """Flag indicating that the expander message relates to a Relay Expander."""
 
+
+    type = None
+    """Expander message type: ExpanderMessage.ZONE or ExpanderMessage.RELAY"""
+    address = -1
+    """Address of expander"""
+    channel = -1
+    """Channel on the expander"""
+    value = -1
+    """Value associated with the message"""
+
     def __init__(self, data=None):
         """
         Constructor
@@ -130,11 +167,7 @@ class ExpanderMessage(BaseMessage):
         :param data: The message data to parse.
         :type data: str
         """
-        self.type = None
-        self.address = None
-        self.channel = None
-        self.value = None
-        self.raw = None
+        BaseMessage.__init__(self)
 
         if data is not None:
             self._parse_message(data)
@@ -176,6 +209,17 @@ class RFMessage(BaseMessage):
     Represents a message from an RF receiver.
     """
 
+    serial_number = None
+    """Serial number of the RF device"""
+    value = -1
+    """Value associated with this message"""
+    battery = False
+    """Battery low indication"""
+    supervision = False
+    """Supervision required indication"""
+    loop = [False for x in range(4)]
+    """Loop indicators"""
+
     def __init__(self, data=None):
         """
         Constructor
@@ -183,12 +227,7 @@ class RFMessage(BaseMessage):
         :param data: The message data to parse
         :type data: str
         """
-        self.raw = None
-        self.serial_number = None
-        self.value = None
-        self.battery = None
-        self.supervision = None
-        self.loop = {}
+        BaseMessage.__init__(self)
 
         if data is not None:
             self._parse_message(data)
@@ -218,7 +257,7 @@ class RFMessage(BaseMessage):
             # Bit 1 = unknown
             self.battery = is_bit_set(2)
             self.supervision = is_bit_set(3)
-            # Bit 8 = unknown
+            # Bit 4 = unknown
             self.loop[0] = is_bit_set(5)
             self.loop[1] = is_bit_set(6)
             self.loop[2] = is_bit_set(7)
@@ -227,11 +266,17 @@ class RFMessage(BaseMessage):
         except ValueError:
             raise InvalidMessageError('Received invalid message: {0}'.format(data))
 
-
 class LRRMessage(BaseMessage):
     """
     Represent a message from a Long Range Radio.
     """
+
+    event_data = None
+    """Data associated with the LRR message.  Usually user ID or zone."""
+    partition = -1
+    """The partition that this message applies to"""
+    event_type = None
+    """The type of the event that occurred"""
 
     def __init__(self, data=None):
         """
@@ -240,10 +285,7 @@ class LRRMessage(BaseMessage):
         :param data: The message data to parse.
         :type data: str
         """
-        self.raw = None
-        self.event_data = None
-        self.partition = None
-        self.event_type = None
+        BaseMessage.__init__(self)
 
         if data is not None:
             self._parse_message(data)
