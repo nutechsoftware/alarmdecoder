cafe2c0a218fd74cecdd450a492138861a1206f0
Scott Petersen <scott@nutech.com>
2013-12-20 16:57:23 -0800

    Filename updates.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 def readme():
-    """Returns the contents of README.md"""
+    """Returns the contents of README.rst"""
 
     with open('README.rst') as readme_file:
         return readme_file.read()
